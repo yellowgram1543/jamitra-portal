dc9a47d5376434e7a50d1d063440e4613d4bc8a6
yellowgram1543 <anushaanand1543@gmail.com>
2026-03-13 12:26:23 +0530

    enable cors

--- janmitra-backend/server.js
+++ janmitra-backend/server.js
@@ -1,31 +1,20 @@
-import express from "express"
-import mongoose from "mongoose"
-import cors from "cors"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-import applicationRoutes from "./routes/applicationRoutes.js"
-import schemeRoutes from "./routes/schemeRoutes.js";
-import reportRoutes from "./routes/reportRoutes.js";
+dotenv.config();
 
-dotenv.config()
+const app = express();
 
-const app = express()
+app.use(cors());
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Janmitra API running");
+});
 
-app.use("/api/applications", applicationRoutes)
-app.use("/api/schemes", schemeRoutes)
-app.use("/api/reports", reportRoutes)
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log("MongoDB Connected"))
-.catch(err=>console.log(err))
-
-app.get("/",(req,res)=>{
-res.send("JanMitra Backend Running")
-})
-
-app.listen(process.env.PORT,()=>{
-console.log("Server running on port 5000")
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
